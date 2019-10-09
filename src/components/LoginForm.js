8ea94cc933d8f587bc595b79564afdd0c400f65a
PollyCR <polly.c.reynolds@gmail.com>
2019-10-09 16:40:48 +0100

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,58 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.username !== "" && this.state.password !== "") {
+      this.props.handleLogin(this.state);
+    }
+  };
+
+  handleUsername = event => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePassword = event => {
+    this.setState({
+      password: event.target.value
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              autoComplete="username"
+              id="username"
+              name="username"
+              type="text"
+              onChange={event => this.handleUsername(event)}
+              value={this.state.username}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              autoComplete="current-password"
+              id="password"
+              name="password"
+              type="password"
+              onChange={event => this.handlePassword(event)}
+              value = {this.state.password}
+            />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,35 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
+
+  calculateRemainingCharacters = () => {
+    // console.log(this)
+        let messageLength = this.state.message.length
+    return this.props.maxChars - messageLength
+  }
+
+  handleMessage = event => {
+    this.setState({
+      message: event.target.value
+    })
+  };
 
   render() {
     return (
-      <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
-      </div>
-    );
-  }
+     <div><input
+      type="text"
+      name="message"
+      id="message"
+      onChange={event => this.handleMessage(event)}
+      value={this.state.message}
+    />
+    <p>Character Count: {this.calculateRemainingCharacters()}</p>
+    </div>
+    )}
 }
 
 export default TwitterMessage;
